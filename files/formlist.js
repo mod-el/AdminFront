778793045db691208d43bfdcc38489634ca5d4d1
Domenico Giambra <sviluppo@editt.it>
2018-04-14 18:43:15 +0200

    Promise based file upload in admin form list visualizer

--- files/formlist.js
+++ files/formlist.js
@@ -5,6 +5,8 @@ function getSublistArray(name) {
 
 	let list = {};
 
+	let promises = [];
+
 	cont.querySelectorAll('input[type="hidden"][name^="ch-' + name + '-"]').forEach(mainInput => {
 		if (mainInput.getValue(true) != '1')
 			return;
@@ -13,20 +15,24 @@ function getSublistArray(name) {
 		if (!row)
 			return;
 
-		let rowArr = {};
+		list[id] = {};
 
 		row.querySelectorAll('input[name^="ch-"], select[name^="ch-"], textarea[name^="ch-"]').forEach(input => {
 			if (input === mainInput)
 				return;
 			let fieldName = input.name.substr(3);
 			fieldName = fieldName.substr(0, fieldName.length - ('-' + name + '-' + id).length);
-			rowArr[fieldName] = input.getValue(true);
+			promises.push(input.getValue().then((fieldName => {
+				return v => {
+					list[id][fieldName] = v;
+				};
+			})(fieldName)));
 		});
-
-		list[id] = rowArr;
 	});
 
-	return list;
+	return Promise.all(promises).then(() => {
+		return list;
+	});
 }
 
 function getDeletedFromSublist(name) {
@@ -56,24 +62,25 @@ function saveFormList() {
 	changeSaveButton();
 	resize();
 
-	let data = getSublistArray('list');
-	let deleted = getDeletedFromSublist('list');
-	return ajax(adminPrefix + currentAdminPage.split('/')[0] + '/saveFormList', 'ajax', 'data=' + encodeURIComponent(JSON.stringify(data)) + '&deleted=' + encodeURIComponent(JSON.stringify(deleted)) + '&c_id=' + c_id).then(r => {
-		saving = false;
-		restoreSaveButton();
+	getSublistArray('list').then(list => {
+		let deleted = getDeletedFromSublist('list');
+		return ajax(adminPrefix + currentAdminPage.split('/')[0] + '/saveFormList', 'ajax', 'data=' + encodeURIComponent(JSON.stringify(list)) + '&deleted=' + encodeURIComponent(JSON.stringify(deleted)) + '&c_id=' + c_id).then(r => {
+			saving = false;
+			restoreSaveButton();
 
-		if (typeof r !== 'object') {
-			alert(r);
-			return false;
-		}
-		if (r.status === 'ok') {
-			return reloadResultsTable().then(() => {
-				inPageMessage('Salvataggio correttamente effettuato.', 'green-message');
-			});
-		} else if (typeof r.err !== 'undefined') {
-			alert(r.err);
-		} else {
-			alert('Generic error');
-		}
+			if (typeof r !== 'object') {
+				alert(r);
+				return false;
+			}
+			if (r.status === 'ok') {
+				return reloadResultsTable().then(() => {
+					inPageMessage('Salvataggio correttamente effettuato.', 'green-message');
+				});
+			} else if (typeof r.err !== 'undefined') {
+				alert(r.err);
+			} else {
+				alert('Generic error');
+			}
+		});
 	});
 }
