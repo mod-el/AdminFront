063568024f046b22138a582fc8d6f729e0375856
Domenico Giambra <sviluppo@editt.it>
2020-06-08 14:41:00 +0200

    Turning off time limit in csv export

--- Controllers/AdminController.php
+++ Controllers/AdminController.php
@@ -128,6 +128,7 @@ class AdminController extends Controller
 										break;
 									default:
 										$this->model->_Log->disableAutoLog();
+										ini_set('max_execution_time', '0');
 
 										$options['perPage'] = $_POST['rows-number'];
 										$options['p'] = $_GET['csv'];
