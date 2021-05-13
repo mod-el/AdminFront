5108926d4f2306196359375da050289b2e37ee6c
Domenico Giambra <zorinik@gmail.com>
2021-05-13 15:41:22 +0200

    Miglior gestione asincronia

--- assets/visualizers/FormList.js
+++ assets/visualizers/FormList.js
@@ -235,7 +235,8 @@ class FormList {
 
 		await form.build(template, data);
 
-		this.rows.set(id, {id, row, form, isNew, deleted: false});
+		let rowObj = {id, row, form, isNew, deleted: false};
+		this.rows.set(id, rowObj);
 		if (isNew)
 			this.newRows.push(id);
 
@@ -254,7 +255,7 @@ class FormList {
 
 			this.refreshLabelsVisibility();
 
-			return id;
+			return rowObj;
 		});
 	}
 
