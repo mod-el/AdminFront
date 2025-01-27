372e0e2419a195e7a39799c228fe1f5c356df021
Domenico Giambra <zorinik@gmail.com>
2025-01-27 20:34:55 +0100

    Fix

--- migrations/20100101000000_users_customizations.php
+++ migrations/20100101000000_users_customizations.php
@@ -7,7 +7,7 @@ class UsersCustomizations extends AbstractMigration
 	public function change()
 	{
 		if (!$this->hasTable('admin_user_customizations')) {
-			$this->table('admin_user_customizations', ['signed' => false])
+			$this->table('admin_user_customizations', ['signed' => true])
 				->addColumn('path', 'string', ['null' => false])
 				->addColumn('user', 'integer', ['null' => false])
 				->addColumn('key', 'string', ['null' => false])
